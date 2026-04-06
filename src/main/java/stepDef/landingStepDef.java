83667859552fc75e0cb836e7c367967ea0116466
gowthamraj <gowthamraj@SWT-LTOP-604>
2026-04-06 15:54:30 +0530

    New update on landing page

--- src/main/java/stepDef/landingStepDef.java
+++ src/main/java/stepDef/landingStepDef.java
@@ -40,14 +40,22 @@ public class landingStepDef {
 
 
 
+		@Given("User is on landing page, if cart button is redirected")
+	public void user_is_on_landing_page_if_cart_button_is_redirected() {
+		land.verifycartButtonRedirection();
+	}
+
+
+
+
+
+
 
 
 	@Given("User is on landing page, if  Profile  button is redirected")
 	public void user_is_on_landing_page_if_profile_button_is_redirected() {
-		land.verifycartButtonRedirection();
-
+		land.verifyProfileiconRedirection();
 	}
-
 
 
 
